cf91d4acd38fdd63b966f9febcbfaf30be3e5341
Rui Molefas <rui@cloudoki.com>
2022-03-06 20:22:57 +0000

    Initial commit

---
+++ src/jarvis-widget-template.ts
@@ -0,0 +1,159 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  LitElement,
+  html,
+  TemplateResult,
+  css,
+  PropertyValues,
+  CSSResultGroup,
+} from 'lit';
+import { property, state } from "lit/decorators";
+import {
+  HomeAssistant,
+  hasConfigOrEntityChanged,
+  hasAction,
+  ActionHandlerEvent,
+  handleAction,
+  LovelaceCardEditor,
+  getLovelace,
+} from 'custom-card-helpers'; // This is a community maintained npm module with common helper functions/types. https://github.com/custom-cards/custom-card-helpers
+import { CARD_VERSION } from './constants';
+import './editor';
+
+import type { BoilerplateCardConfig } from './types';
+import { actionHandler } from './action-handler-directive';
+
+/* eslint no-console: 0 */
+console.info(
+  `%c  JARVIS-WIDGET-TEMPLATE \n%c  version: ${CARD_VERSION}  `,
+  'color: orange; font-weight: bold; background: black',
+  'color: white; font-weight: bold; background: dimgray',
+);
+
+(window as any).customCards = (window as any).customCards || [];
+(window as any).customCards.push({
+  type: 'jarvis-widget-template',
+  name: 'Boilerplate Card',
+  description: 'A template custom card for you to create something awesome',
+});
+
+export class BoilerplateCard extends LitElement {
+  public static async getConfigElement(): Promise<LovelaceCardEditor> {
+    // REPLACE "jarvis-widget-template" with widget name, everywhere in the project
+    // REPLACE the file name with the actual widget name
+    return document.createElement('jarvis-widget-template-editor');
+  }
+
+  public static getStubConfig(): Record<string, unknown> {
+    return {};
+  }
+
+  @property({ attribute: false }) public hass!: HomeAssistant;
+  @state() private config!: BoilerplateCardConfig;
+
+  public setConfig(config: BoilerplateCardConfig): void {
+    // TODO Check for required fields and that they are of the proper format
+    if (!config) {
+      throw new Error('Invalid configuration');
+    }
+
+    if (config.test_gui) {
+      getLovelace().setEditMode(true);
+    }
+
+    this.config = {
+      name: 'Boilerplate',
+      ...config,
+    };
+  }
+
+  protected shouldUpdate(changedProps: PropertyValues): boolean {
+    if (!this.config) {
+      return false;
+    }
+
+    return hasConfigOrEntityChanged(this, changedProps, false);
+  }
+
+  private _handleAction(ev: ActionHandlerEvent): void {
+    if (this.hass && this.config && ev.detail.action) {
+      handleAction(this, this.hass, this.config, ev.detail.action);
+    }
+  }
+
+  private _showWarning(warning: string): TemplateResult {
+    return html`
+      <hui-warning>${warning}</hui-warning>
+    `;
+  }
+
+  private _showError(error: string): TemplateResult {
+    const errorCard = document.createElement('hui-error-card');
+    errorCard.setConfig({
+      type: 'error',
+      error,
+      origConfig: this.config,
+    });
+
+    return html`
+      ${errorCard}
+    `;
+  }
+
+  static get styles(): CSSResultGroup {
+    return css`
+      /* REPLACE "jarvis-widget-template" with actual widget name */
+      .type-custom-jarvis-widget-template {
+        height: 100%;
+        width: 100%;
+      }
+      .jarvis-widget {
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        padding: 20px;
+        box-sizing: border-box;
+        border: 1px solid #fff;
+      }
+    `;
+  }
+
+  protected render(): TemplateResult | void {
+    /*
+      ## INTERFACE
+      - this.hass: A lot of information about everything in HA, such as states, theme, etc. The source of the tree
+        - states: States of each of the components available
+      - this.config: Lovelace settings for this instance
+
+      Example: this.hass.states[this.config.entities[0]] shows the state of the first component
+     */
+
+    // TODO Check for stateObj or other necessary things and render a warning if missing
+    if (this.config.show_warning) {
+      return this._showWarning('warning message');
+    }
+
+    if (this.config.show_error) {
+      return this._showError('error message');
+    }
+
+    return html`
+      <ha-card
+        .header=${this.config.name}
+        @action=${this._handleAction}
+        .actionHandler=${actionHandler({
+          hasHold: hasAction(this.config.hold_action),
+          hasDoubleClick: hasAction(this.config.double_tap_action),
+        })}
+        tabindex="0"
+        .label=${`Boilerplate: ${this.config || 'No Entity Defined'}`}
+      >
+        <div class='jarvis-widget'>It's the template!</div>
+      </ha-card>
+    `;
+  }
+}
+
+customElements.define("jarvis-widget-template", BoilerplateCard);
