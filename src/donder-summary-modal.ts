bd0e7e9d0867b022ae5cbf092b8b6d0b978e94f5
Muffles <ruimolefas@Ruis-MBP.lan>
2023-12-16 13:05:18 +0000

    build

--- src/donder-summary-modal.ts
+++ src/donder-summary-modal.ts
@@ -32,7 +32,7 @@ console.info(
 (window as any).customCards = (window as any).customCards || [];
 (window as any).customCards.push({
   type: 'donder-summary-modal',
-  name: 'Boilerplate Card',
+  name: 'Donder Summary Modal',
   description: 'A template custom card for you to create something awesome',
 });
 
