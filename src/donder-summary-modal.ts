2f4de5d127088486e71de163d76eb11d96d0c2d5
Muffles <ruimolefas@Ruis-MBP.lan>
2024-01-06 17:09:55 +0000

    build

--- src/donder-summary-modal.ts
+++ src/donder-summary-modal.ts
@@ -276,7 +276,7 @@ export class BoilerplateCard extends LitElement {
     clearTimeout(this._throttle);
     
     this._throttle = setTimeout(() => {
-      window.alert(`triggered ${percentage} -> ${next}, ${JSON.stringify(element)}`)
+      window.alert(`triggered ${percentage} -> ${next}, ${element.toString()}`)
       this.hass.callService('cover', 'set_cover_position', {entity_id: sw.entity, position: next})
     }, 2000)
   }
